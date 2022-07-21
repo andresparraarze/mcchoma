5c12c11f7e242eef314b3772c31fe6560b46090f
Parra <andresparraarze@gmail.com>
2022-07-21 01:43:40 -0400

    addition home main and tailwind css edits

--- src/components/Homemain.jsx
+++ src/components/Homemain.jsx
@@ -3,8 +3,23 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Homemain = () => {
   return (
-    <div className='w-full h-screen bg-[#19191b]'>
+    <div name='homemain' className='w-full h-screen bg-[#19191b]'>
 
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-2xl text-yellow-400'>Hello I am</p>
+            <h1 className='text-4xl sm:text-6xl font-bold text-gray-200'>Andres Parra Arze</h1>
+            <h2 className='text-3xl sm:text-5xl font-bold text-gray-400'>Full Stack Web Developer </h2>
+            <p className='text-gray-200 py-4 max-w-[700px]'>I specialize in the front end area and make very interactive experiences,
+                i can make full stack web applications with very modern up to date resources
+            </p>
+            <div>
+                <button className='text-white group border-2 px-5 py-2 my-2 flex items-center hover:bg-yellow-500 hover:border-yellow-500 hover:px-2 duration-300 hover:py-3'> Projects 
+                    <span className='group-hover:rotate-90 duration-300'>
+                        <HiArrowNarrowRight className='ml-4'/>
+                    </span>
+                </button>
+            </div>
+        </div>
     </div>
   )
 }
